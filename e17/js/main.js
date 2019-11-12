b5e5df77b94184a60238b348af9dfbbcf0ad34bd
Kerrigan Patterson <kerriganpatterson@aol.com>
2019-11-12 18:34:58 -0500

    e17

--- e17/js/main.js
+++ e17/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
   console.log('Hewwo');
+
+  $( '.chocolate' ).click(function() {
+    $('.numone').toggleClass('align-left')
+  });
+
+  $( '.potatoes' ).click(function() {
+    $('.numtwo').toggleClass('big')
+  });
+
+  $( '.ramen' ).click(function() {
+    $('.numthree').toggleClass('color')
+  });
 });
